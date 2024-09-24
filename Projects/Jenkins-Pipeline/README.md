45394949be62bf1b2e9efe3398709c7d281dcf2a
Tushar Singh Bisht <tusharbisht2011@gmail.com>
2024-09-24 13:51:48 +0530

    Initail commit

---
+++ Projects/Jenkins-Pipeline/README.md
@@ -0,0 +1,55 @@
+This projects includes:
+
+Deploying an ec2 instance, install Jenkins in the instance, configure Docker cagent adn containers, set up cicd, deploy applications to k8s.
+
+Run the below commands to install Java and Jenkins:
+
+Install Java
+'''
+    sudo apt update
+    sudo apt install openjdk-17-jre
+'''
+
+Verify Java is Installed
+'''
+    java -version
+'''
+
+Now, you can proceed with installing Jenkins
+
+'''
+    curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee \
+    /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+    echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+    https://pkg.jenkins.io/debian binary/ | sudo tee \
+    /etc/apt/sources.list.d/jenkins.list > /dev/null
+    sudo apt-get update
+    sudo apt-get install jenkins
+'''
+
+Install jenkins
+
+Install Docker:
+'''
+    sudo apt install docker.io
+'''
+
+Grant Jenkins user and Ubuntu user permission to docker deamon.
+'''
+    sudo su - 
+'''
+
+Give jenkins and ubuntu user permission for docker deamon.
+'''
+    usermod -aG docker jenkins
+    usermod -aG docker ubuntu
+    systemctl restart docker
+'''
+
+Once you are done with the above steps, it is better to restart Jenkins.
+'''
+    http://<ec2-instance-public-ip>:8080/restart
+'''
+
+Install docker pipeline plugin in jenkins.
+
---
+++ Projects/Jenkins-Pipeline/main.tf
@@ -0,0 +1,57 @@
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_key_pair" "terra_jenkins_pub_ubun" {
+  key_name   = "terra_jenkins-pub-ubun"
+  public_key = file("~/.ssh/id_rsa.pub")  # Replace with the path to your public key file
+}
+
+# ssh -i ~/.ssh/id_rsa ubuntu@
+
+resource "aws_security_group" "terra_jenkins_sg" {
+  vpc_id = "vpc-09caa6ee9c5b42fa5"
+
+  ingress {
+    description = "Allow SSH into instance"
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow access through port 8080 into instance"
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow all traffi to eixt"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = [ "0.0.0.0/0" ]
+  }
+
+  tags = {
+    Name = "terra-jenkins-sg"
+  }
+}
+
+resource "aws_instance" "jenkins_instance" {
+  ami                     = "ami-0e86e20dae9224db8"
+  instance_type           = "t2.micro"
+  key_name                = aws_key_pair.terra_jenkins_pub_ubun.key_name
+  vpc_security_group_ids  = [aws_security_group.terra_jenkins_sg.id]
+  tags = {
+    Name = "jenkins-instance"
+  }
+}
+
+output "instance_public_ip" {
+  value = aws_instance.jenkins_instance.public_ip
+}
